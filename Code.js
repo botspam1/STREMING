ab3f4b05a79b9b2165e84d92c24d38d40a639f0d
botspam1 <49961396+botspam1@users.noreply.github.com>
2019-04-24 23:28:21 -1200

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,7 @@
+﻿const Discord = require("discord.js");
+const MooDY = new Discord.Client();
+MooDY.on('ready', () => {
+  MooDY.user.setGame(`عندي شغل لحد يكلمني`,'https://www.twitch.tv/iiMooDY9');
+  console.log("["+MooDY.user.username+"] Online now !!");
+});
+MooDY.login("NDU1MDY1MjE3OTAzMzYyMDQ5.XL38fQ.RbwtQ-MgWcn367Jd55DfuskR29E"); // <= Your token here !!
